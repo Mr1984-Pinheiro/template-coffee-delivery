5c2fbf49a6a565c1de51fa0f3d383aa5fdbbb977
carlospinheiro <cadutapinheiro@gmail.com>
2023-07-25 19:53:16 -0300

    create cart context

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,22 @@
-import { BrowserRouter} from "react-router-dom"
+import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 import { defaultTheme } from "./styles/themes/default";
 import { GlobalStyle } from "./styles/global";
 import { Router } from "./Routes";
-import "./index.css"
-
+import "./index.css";
+import { CartContextProvider } from "./contexts/CartContext";
 
 function App() {
-  
-
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
-        <BrowserRouter>
+      <BrowserRouter>
+        <CartContextProvider>
           <Router />
-        </BrowserRouter>      
-    </ThemeProvider>   
-  )
+        </CartContextProvider>
+      </BrowserRouter>
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
