9137b494090734671dfe833ddf53e66ae1a25e26
carlospinheiro <cadutapinheiro@gmail.com>
2023-08-03 14:23:16 -0300

    add form provider

--- src/pages/CompleteOrder/index.tsx
+++ src/pages/CompleteOrder/index.tsx
@@ -1,11 +1,37 @@
+import { FormProvider, useForm } from "react-hook-form";
 import { CompleteOrderform } from "./components/CompleteOrderForm";
 import { SelectedCoffees } from "./components/SelectedCoffees";
+import * as zod from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const confirmOrderFormValidationSchema = zod.object({
+  cep: zod.string(),
+});
+
+export type OrderData = zod.infer<typeof confirmOrderFormValidationSchema>;
+
+type ConfirmOrderFormData = OrderData;
 
 export function CompleteOrderPage() {
+  const confirmOrderForm = useForm<ConfirmOrderFormData>({
+    resolver: zodResolver(confirmOrderFormValidationSchema),
+  });
+
+  const { handleSubmit } = confirmOrderForm;
+
+  function handleConfirmOrder(data: ConfirmOrderFormData) {
+    console.log(data);
+  }
+
   return (
-    <form className="container mt-10 flex-col flex lg:flex-row justify-between gap-8">
-      <CompleteOrderform />
-      <SelectedCoffees />
-    </form>
+    <FormProvider {...confirmOrderForm}>
+      <form
+        className="container mt-10 flex-col flex lg:flex-row justify-between gap-8"
+        onSubmit={handleSubmit(handleConfirmOrder)}
+      >
+        <CompleteOrderform />
+        <SelectedCoffees />
+      </form>
+    </FormProvider>
   );
 }
