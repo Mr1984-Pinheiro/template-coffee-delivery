db6e969b3e78936f502331c2403644c8acb5f125
carlospinheiro <cadutapinheiro@gmail.com>
2023-08-04 14:32:35 -0300

    fix complete order

--- src/pages/CompleteOrder/index.tsx
+++ src/pages/CompleteOrder/index.tsx
@@ -1,8 +1,55 @@
-import { FormProvider, useForm } from "react-hook-form";
 import { CompleteOrderform } from "./components/CompleteOrderForm";
 import { SelectedCoffees } from "./components/SelectedCoffees";
+import { useForm, FormProvider } from "react-hook-form";
+import * as zod from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useNavigate } from "react-router-dom";
+import { useCart } from "../../hooks/useCart";
+
+enum PaymentMethods {
+  credit = "credit",
+  debit = "debit",
+  money = "money",
+}
+
+const confirmOrderFormValidationSchema = zod.object({
+  cep: zod.string().min(1, "Informe o CEP"),
+  street: zod.string().min(1, "Informe a Rua"),
+  number: zod.string().min(1, "Informe o Número"),
+  complement: zod.string(),
+  district: zod.string().min(1, "Informe o Bairro"),
+  city: zod.string(),
+  uf: zod.string(),
+  paymentMethod: zod.nativeEnum(PaymentMethods, {
+    errorMap: () => {
+      return { message: "Informe o método de pagamento" };
+    },
+  }),
+});
+
+export type OrderData = zod.infer<typeof confirmOrderFormValidationSchema>;
+
+type ConfirmOrderFormData = OrderData;
 
 export function CompleteOrderPage() {
+  const confirmOrderForm = useForm<ConfirmOrderFormData>({
+    resolver: zodResolver(confirmOrderFormValidationSchema),
+    defaultValues: {
+      paymentMethod: undefined,
+    },
+  });
+
+  const { handleSubmit } = confirmOrderForm;
+
+  const navigate = useNavigate();
+  const { cleanCart } = useCart();
+
+  function handleConfirmOrder(data: ConfirmOrderFormData) {
+    navigate("/orderConfirmed", {
+      state: data,
+    });
+    cleanCart();
+  }
   return (
     <FormProvider {...confirmOrderForm}>
       <form
